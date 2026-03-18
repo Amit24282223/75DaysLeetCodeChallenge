9cfccf217a24862e3b3502d7559465e5dfddea53
Amit24282223 <amitmauraya1312@gmail.com>
2026-03-18 17:44:35 +0530

    Time: 1 ms (98.85%), Space: 47.7 MB (17.09%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int countBattleships(char[][] grid) {
+         int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+        boolean check[][] = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!check[i][j] && grid[i][j] == 'X') {
+                    check(grid, check, j, i);
+                    count++;
+                }
+            }
+        }
+        return count;
+
+    }
+    // public void bfs(int grid[][],boolean check[][],int i,int j){
+
+    // }
+    public static void check(char grid[][], boolean check[][], int c, int r) {
+        if (c >= grid[0].length || r >= grid.length) {
+            return;
+        }
+         if (c < 0 || r < 0||grid[r][c]=='.') {
+            return;
+        }
+        if (grid[r][c] == 'X' && check[r][c] == false) {
+            check[r][c] = true;
+        }
+        if (r - 1 >= 0 && grid[r - 1][c] =='X' && check[r - 1][c] == false) {
+            check[r - 1][c] = true;
+            check(grid, check, c, r - 1);
+        }
+        if (c - 1 >= 0 && grid[r][c - 1] == 'X' && check[r][c - 1] == false) {
+            check[r][c - 1] = true;
+            check(grid, check, c - 1, r);
+
+        }
+        if (r + 1 < grid.length && grid[r + 1][c] =='X' && check[r + 1][c] == false) {
+            check[r + 1][c] = true;
+            check(grid, check, c, r + 1);
+
+        }
+        if (c + 1 < grid[0].length && grid[r][c + 1] == 'X' && check[r][c + 1] == false) {
+            check[r][c + 1] = true;
+            check(grid, check, c + 1, r);
+        }
+    }
+
+}
